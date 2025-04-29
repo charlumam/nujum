3230536cb7b1a299f6bd4a4e503a5bec46347732
Charda Lufthansa Umam <charlumam@gmail.com>
2025-04-29 19:03:34 +0700

    Improved favicon using astro-favicons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import favicons from "astro-favicons";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
+  integrations: [react(), favicons()],
 });
