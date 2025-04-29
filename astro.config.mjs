0ad23e81646cf9cdc184c7749fbc615c57b60411
Charda Lufthansa Umam <charlumam@gmail.com>
2025-04-30 01:16:05 +0700

    Added Google Analytics with Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import favicons from "astro-favicons";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react(), favicons()],
+  integrations: [react(), favicons(), partytown()],
 });
