a2336d530919574c965f7152e75f8a8884b94777
Charda Lufthansa Umam <charlumam@gmail.com>
2026-01-07 18:06:24 +0700

    Add wrangler and Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,17 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
-import favicons from "astro-favicons";
 import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
+import favicons from "astro-favicons";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nujum.charda.org",
+
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [react(), favicons(), sitemap()],
+  adapter: cloudflare(),
 });
