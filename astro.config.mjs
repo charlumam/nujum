dd545ad5c4f6e2a8208da7df518b2939c5e8aa20
Charda Lufthansa Umam <charlumam@gmail.com>
2026-01-19 13:07:10 +0700

    Add and configure for Clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
+import clerk from "@clerk/astro";
 import cloudflare from "@astrojs/cloudflare";
 import favicons from "astro-favicons";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://nujum.charda.org",
@@ -12,6 +13,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react(), favicons(), sitemap()],
+  integrations: [react(), favicons(), sitemap(), clerk()],
   adapter: cloudflare(),
 });
